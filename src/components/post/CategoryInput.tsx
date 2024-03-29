0f8d8aa3cb75d393e3c13637a4b7e0161adc8aba
JIHYE PARK <79756267+j-wisdom-h@users.noreply.github.com>
2024-03-29 16:16:31 +0900

    Feature/post: 프로젝트 생성 API(단, 이미지 제외), 카테고리 조회, 카테고리 생성 API, 맴버조회 API (#11)
    Feat: 프로젝트 생성 API(단, 이미지 제외) 및 제목/설명/깃링크의 상태를 ref로 관리하도록 변경
    Feat: 카테고리 조회, 카테고리 생성 API
    Feat: 맴버조회 API
    Feat: Icategory, Imember 추가 및 PostFormData의 projectCategories, teamProjectMembers타입 변경
    Refactor: SelctedStyledList컴포넌트에 제네릭타입을 추가하여 공통으로 사용할 수 있는 컴포넌트로 변경
    Feat: TagStyeldList구현
    Feat: localStorage에서 token을 get,set하는 utils함수

--- src/components/post/CategoryInput.tsx
+++ src/components/post/CategoryInput.tsx
@@ -1,42 +1,90 @@
 import { useEffect, useRef, useState } from "react";
 import { Section, SectionCol } from "../SectionDirection";
-import InputWithEnter from "./InputWithEnter";
 import { Label } from "../CommonTag";
-import StyledList from "./StyledList";
+import StyledList from "./SelectedStyledList";
+import TagStyledList from "./TagStyledList";
+import InputWithEnter from "./InputWithEnter";
+import { Icategory } from "../../interfaces/IPostFormData";
+import { getToken } from "../../utils/token";
 import styled from "styled-components";
+import axios from "axios";
 
 interface Prop {
-  onChangeCategory: (categories: string[]) => void;
+  onChangeCategory: (categories: Icategory[]) => void;
 }
+
 const CategoryInput = ({ onChangeCategory }: Prop) => {
   const categoryRef = useRef<HTMLInputElement>(null);
-  const [categories, setCategories] = useState<string[]>([]);
+  const [categories, setCategories] = useState<Icategory[]>();
+  const [selectedCategories, setSelectedCategories] = useState<Icategory[]>([]);
 
-  const handleAddCategory = (newcategory: string) => {
-    setCategories([...categories, newcategory]);
+  // memo지혜: 사용자 카테고리 생성
+  const handleAddCategory = async (name: string) => {
+    const res = await axios.post(
+      `${process.env.REACT_APP_API_URL}/category/create`,
+      { name },
+      {
+        headers: {
+          Authorization: `${getToken()}`,
+          "Content-Type": "application/json;charset=UTF-8",
+        },
+      }
+    );
+    const createdId = res.data.id;
+    setSelectedCategories([...selectedCategories, { id: createdId, name }]);
   };
 
-  function handleRemoveCategory(category: string) {
-    setCategories(categories.filter((c) => c !== category));
+  function handleRemoveCategory(categoryId: number) {
+    setSelectedCategories(
+      selectedCategories.filter((c) => c.id !== categoryId)
+    );
   }
 
+  function selectCategory(categoryId: number, categoryName: string) {
+    setSelectedCategories((prev) => [
+      ...prev,
+      { id: categoryId, name: categoryName },
+    ]);
+  }
+
+  //memo지혜: lifting state up
   useEffect(() => {
-    onChangeCategory(categories);
-  }, [categories]);
+    onChangeCategory(selectedCategories);
+  }, [selectedCategories]);
+
+  // memo지혜: 카테고리 조회
+  useEffect(() => {
+    async function featchData() {
+      const res = await axios.get(
+        `${process.env.REACT_APP_API_URL}/category/list`
+      );
+      const result = res.data;
+      if (result.length > 0) {
+        setCategories([...result]);
+      }
+    }
+    featchData();
+  }, []);
 
   return (
     <Section>
       <Label>카테고리</Label>
       <SectionCol>
+        {categories && (
+          <TagStyledList list={categories} onSelect={selectCategory} />
+        )}
         <InputWithEnter
           placeholder="카테고리를 입력하고 ENTER을 눌러주세요."
           onEnter={handleAddCategory}
           ref={categoryRef}
         />
-        {categories.length === 0 ? (
+        {selectedCategories.length === 0 ? (
           <EmptyInfo>입력된 카테고리가 없습니다.</EmptyInfo>
         ) : (
-          <StyledList lists={categories} handleRemove={handleRemoveCategory} />
+          <StyledList
+            lists={selectedCategories}
+            handleRemove={handleRemoveCategory}
+          />
         )}
       </SectionCol>
     </Section>
---
+++ src/components/post/SelectedStyledList.tsx
@@ -0,0 +1,40 @@
+import styled from "styled-components";
+import { Icategory, Imember } from "../../interfaces/IPostFormData";
+import { Item, List } from "../CommonTag";
+
+// memo지혜: 선택된 카테고리와 맴버를 나열하는데 공통을 사용되는 컴포넌트
+
+type ListItem = Icategory | Imember;
+
+interface ListProps<T extends ListItem> {
+  lists: T[];
+  handleRemove: (list: T["id"]) => void;
+}
+
+const StyledList = ({ lists, handleRemove }: ListProps<ListItem>) => {
+  return (
+    <List>
+      {lists.length > 0 && (
+        <>
+          {lists.map((list) => (
+            <Item key={list.id}>
+              <span>{"name" in list ? list.name : list.nickname}</span>
+              <DeleteButton onClick={() => handleRemove(list.id)}>
+                x
+              </DeleteButton>
+            </Item>
+          ))}
+        </>
+      )}
+    </List>
+  );
+};
+
+export default StyledList;
+
+const DeleteButton = styled.span`
+  padding-left: 1.5rem;
+  font-weight: bold;
+  cursor: pointer;
+  font-size: 1.2rem;
+`;
