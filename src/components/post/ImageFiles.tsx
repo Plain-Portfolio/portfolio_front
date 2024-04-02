415a9908afa9c62e00dd455e7e4d8102abf55969
bgs-study <qkrwlgp235@gmail.com>
2024-04-02 16:21:43 +0900

    Feat: 이미지 업로드API, 수정모드시 이미지를 preview에 props로 전달

--- src/components/post/ImageFiles.tsx
+++ src/components/post/ImageFiles.tsx
@@ -1,10 +1,16 @@
 import React, { useEffect, useRef, useState } from "react";
 import { SectionCol, SectionRow } from "../SectionDirection";
-import { Button, Input } from "../CommonTag";
+import { BorderButton, Input } from "../CommonTag";
 import styled from "styled-components";
 import ImagePreview from "./ImagePreview";
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { IdNumberArr, IprojectImgs } from "../../interfaces/IPost";
 
-type Props = { onFilesChange: (files: File[]) => void };
+type Props = {
+  onFilesChange: (files: IdNumberArr) => void;
+  defaultimages: IprojectImgs[] | undefined;
+};
 
 const ALLOW_FILE_EXTENSION = "jpg,jpeg,png";
 const FILE_SIZE_MAX_LIMIT = 5 * 1024 * 1024; // 5MB
@@ -36,11 +42,52 @@ const renderFileNames = (validFiles: File[]) => {
     .join(", ")}외 ${validFiles.length - MAX_FILE_NAME_VIEW}개`;
 };
 
-const ImageFiles = ({ onFilesChange }: Props) => {
-  const imageInputRef = useRef<HTMLInputElement>(null);
-  const [fileName, setFileName] = useState<string>("");
-  const [files, setFiles] = useState<File[] | null>(null);
+const uploadImages = async (files: File[]) => {
+  console.log(files);
+  const imageData = new FormData();
+  if (files) {
+    files.forEach((file) => {
+      imageData.append("images", file);
+    });
+  }
 
+  const res = await axios.post(
+    `${process.env.REACT_APP_API_URL}/image/upload`,
+    imageData,
+    { headers: { "Content-Type": "multipart/form-data" } }
+  );
+
+  return res.data;
+};
+
+const ImageFiles = ({ onFilesChange, defaultimages }: Props) => {
+  const imageInputRef = useRef<HTMLInputElement>(null);
+
+  //memo 지혜: 이미지 파일명
+  const [fileName, setFileName] = useState<string>("");
+  //memo 지혜 : 이미지 파일 아이디 배열( 상위 컴포넌트로 lifting up . 폼제출용)
+  const [filesIds, setFilesIds] = useState<{ id: number }[]>([]);
+  //memo 지혜: id와 src배열 ( 업로드 성공시 리턴 값 또는 수정모드일때 받아온 이미지 데이터 )
+  const [uploaded, setUploaded] = useState<IprojectImgs[]>([]);
+  //memo 지혜: 업로드 파일배열
+  const [files, setFiles] = useState<File[]>([]);
+
+  const { mutate: uploadMutate } = useMutation({
+    mutationFn: () => uploadImages(files),
+    onSuccess: (data) => {
+      // memo지혜: 업로드 성공시 리턴 값 상태에 할당 후, 이미지프리뷰에 사용
+      const imageArr = data as IprojectImgs[];
+      setUploaded(imageArr);
+
+      // memo지혜: 해당 게시글에 속한 이미지 파일 id 배열
+      const idArray = imageArr.map(({ id }) => ({ id }));
+      setFilesIds(idArray);
+    },
+    onError: (error) => {
+      console.error("Error uploadImages:", error);
+    },
+  });
+  // memo지혜: 이미지 업로드 함수
   const handleUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     const target = e.currentTarget;
     const files = target.files as FileList;
@@ -50,6 +97,7 @@ const ImageFiles = ({ onFilesChange }: Props) => {
     }
     const FilesToArray = Array.from(files);
 
+    // memo지혜: 이미지 validation
     const validFiles = FilesToArray.filter((file) => {
       if (!fileExtensionValid(file)) {
         alert(
@@ -63,25 +111,38 @@ const ImageFiles = ({ onFilesChange }: Props) => {
       }
       return true;
     });
-
     setFileName(renderFileNames(validFiles));
+    // memo지혜: File[]형식의 이미지 파일을 상태에 할당
     setFiles(validFiles);
+
+    // memo지혜: 이미지 업로드 API trigger
+    uploadMutate();
   };
 
+  // memo지혜: 업로드 버튼 클릭시 이미지 인풋 open
   function handleFileButtonClick() {
     if (imageInputRef.current) {
       imageInputRef.current.click();
     }
   }
 
+  // memo지혜: 지우기버튼 클릭시 초기화
   function handleReomveFiles() {
     setFileName("");
-    setFiles(null);
+    setFilesIds([]);
   }
 
+  // memo지혜: 업로드완료시 id에 대한 배열을 lifting up state
   useEffect(() => {
-    if (files) onFilesChange(files);
-  }, [files]);
+    onFilesChange(filesIds);
+  }, [filesIds]);
+
+  // memo지혜: 수정 모드시 이미지의 데이터 셋팅
+  useEffect(() => {
+    if (defaultimages) {
+      setUploaded(defaultimages);
+    }
+  }, [defaultimages]);
 
   return (
     <SectionCol>
@@ -115,9 +176,12 @@ const ImageFiles = ({ onFilesChange }: Props) => {
       </SectionRow>
       <SectionRow>
         <ImagePreviewBoard>
-          <label>이미지프리뷰</label>{" "}
-          {!files && <p>업로드된 이미지가 없습니다.</p>}
-          {files && <ImagePreview files={files} />}
+          <label>이미지프리뷰</label>
+          {uploaded ? (
+            <ImagePreview files={uploaded} />
+          ) : (
+            <p>업로드된 이미지가 없습니다.</p>
+          )}
         </ImagePreviewBoard>
       </SectionRow>
     </SectionCol>
@@ -126,7 +190,7 @@ const ImageFiles = ({ onFilesChange }: Props) => {
 const ImageInput = styled.input`
   display: none;
 `;
-const FileButton = styled(Button)`
+const FileButton = styled(BorderButton)`
   margin-left: 1rem;
 `;
 const ImagePreviewBoard = styled.div`
