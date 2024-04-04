255986077ba0f0e91332164b6353ed566db90379
moil <moil1672@naver.com>
2024-04-04 15:21:06 +0900

    Feat: 카테고리 기능추가
    유저 프로젝트 리스트 화면에서 카테고리별로 프로젝트 조회할 수 있도록
    기능 추가

--- src/pages/MyProjectList.tsx
+++ src/pages/MyProjectList.tsx
@@ -10,7 +10,7 @@ const fetchProjectData = async (userId: string) => {
     const response = await axios.get<ProjectData[]>(
       `${process.env.REACT_APP_API_URL}/project/${userId}/projects`
     );
-    console.log(response);
+    // console.log(response);
     return response.data;
   } catch (error) {
     console.log("Error fetching projects: ", error);
@@ -23,22 +23,34 @@ function MyProject() {
   const [selectedProjectIdx, setSelectedProjectIdx] = useState<number | null>(
     null
   );
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([]);
 
+  // memo지은: Home에서 userList.id 넘겨받아 해당 유저의 리스트 보여주도록 변경 필요
   const userId = localStorage.getItem("user_id") as string;
   const navigate = useNavigate();
 
   useEffect(() => {
-    const fetchProjects = async () => {
+    const fetchProjects = async (userId: string) => {
       const projectData = await fetchProjectData(userId);
       setProjects(projectData);
     };
     if (userId) {
-      fetchProjects();
+      fetchProjects(userId);
     }
   }, [userId]);
 
   const handleShowSummary = (idx: number) => {
     setSelectedProjectIdx(idx === selectedProjectIdx ? null : idx);
+  };
+
+  const handleCategoryClick = (category: string) => {
+    setSelectedCategories((prevSelectedCategories) => {
+      if (prevSelectedCategories.includes(category)) {
+        return prevSelectedCategories.filter((c) => c !== category);
+      } else {
+        return [...prevSelectedCategories, category];
+      }
+    });
   };
 
   return (
@@ -49,33 +61,64 @@ function MyProject() {
         <MyProfile>Contact To &rarr;</MyProfile>
       </MyProfileWrapper>
 
+      <CategoryWrapper>
+        {dummyDatas
+          .flatMap((project) => project.projectCategories)
+          .filter(
+            (category, index, self) =>
+              index === self.findIndex((c) => c.name === category.name)
+          )
+          .map((category, idx) => (
+            <CategoryButton
+              key={idx}
+              onClick={() => handleCategoryClick(category.name)}
+              active={selectedCategories.includes(category.name) ? 1 : 0}
+            >
+              {category.name}
+            </CategoryButton>
+          ))}
+      </CategoryWrapper>
+
       <MyProjectWrapper>
-        {projects.length > 0 ? (
-          projects.map((project, idx) => {
-            return (
-              <MyProjectList key={idx}>
-                <ProjectContainer>
-                  <ProjectList onClick={() => handleShowSummary(idx)}>
-                    {project.title}
-                  </ProjectList>
-                  <MoveToDetail
-                    onClick={() => navigate(`/read/${project.projectId}`)}
-                  >
-                    구경하러 가기 &rarr;
-                  </MoveToDetail>
-                </ProjectContainer>
-                {selectedProjectIdx === idx && (
-                  <ProjectSummary
-                    style={{
-                      display: selectedProjectIdx === idx ? "block" : "none",
-                    }}
-                  >
-                    {project.description}
-                  </ProjectSummary>
-                )}
-              </MyProjectList>
-            );
-          })
+        {dummyDatas.length > 0 ? (
+          dummyDatas
+            .filter((project) =>
+              selectedCategories.length === 0
+                ? true
+                : project.projectCategories.some((category) =>
+                    selectedCategories.includes(category.name)
+                  )
+            )
+            .map((project, idx) => {
+              return (
+                <>
+                  <ProjectContainer key={idx}>
+                    <ProjectTitle onClick={() => handleShowSummary(idx)}>
+                      {project.title}
+                    </ProjectTitle>
+                    <MoveToDetail
+                      onClick={() => navigate(`/read/${project.projectId}`)}
+                    >
+                      구경하러 가기 &rarr;
+                    </MoveToDetail>
+                  </ProjectContainer>
+                  {selectedProjectIdx === idx && (
+                    <ProjectSummaryContainer
+                      style={{
+                        display: selectedProjectIdx === idx ? "block" : "none",
+                      }}
+                    >
+                      <ProjectSummary>{project.description}</ProjectSummary>
+                      {project.isTeamProject === true ? (
+                        <ProjectSummary>팀 프로젝트</ProjectSummary>
+                      ) : (
+                        <ProjectSummary>개인 프로젝트</ProjectSummary>
+                      )}
+                    </ProjectSummaryContainer>
+                  )}
+                </>
+              );
+            })
         ) : (
           <ProjectContainer>아직 프로젝트가 없습니다...</ProjectContainer>
         )}
@@ -84,44 +127,118 @@ function MyProject() {
   );
 }
 
+const dummyDatas = [
+  {
+    projectId: 0,
+    title: "프로젝트1",
+    description: "JavaScript, TypeScript, React",
+    img: 1, // memo지은: 이미지 아이디로 지급되면 이미지 가져와서 쓰면 됨
+    isTeamProject: true,
+    projectCategories: [
+      { name: "JavaScript", id: 0 },
+      { name: "TypeScript", id: 1 },
+      { name: "React", id: 2 },
+    ],
+  },
+  {
+    projectId: 1,
+    title: "프로젝트2",
+    description: "React, SpringBoot",
+    img: 2,
+    isTeamProject: true,
+    projectCategories: [
+      { name: "React", id: 2 },
+      { name: "SpringBoot", id: 4 },
+    ],
+  },
+  {
+    projectId: 2,
+    title: "프로젝트3",
+    description: "React, Java, SpringBoot",
+    img: 3,
+    isTeamProject: false,
+    projectCategories: [
+      { name: "React", id: 2 },
+      { name: "Java", id: 3 },
+      { name: "SpringBoot", id: 4 },
+    ],
+  },
+  {
+    projectId: 4,
+    title: "프로젝트4",
+    description: "Vanila JavaScript",
+    img: 4,
+    isTeamProject: false,
+    projectCategories: [{ name: "JavaScript", id: 0 }],
+  },
+  {
+    projectId: 5,
+    title: "프로젝트5",
+    description: "JavaScript, TypeScript",
+    img: 5,
+    isTeamProject: false,
+    projectCategories: [
+      { name: "JavaScript", id: 0 },
+      { name: "TypeScript", id: 1 },
+    ],
+  },
+];
+
 const MyProjectPage = styled.div`
-  height: 100%;
+  height: auto;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
-  margin-top: 100px;
+  padding-top: 100px;
+  padding-bottom: 100px;
 `;
 
 const MyProfileWrapper = styled.div`
   display: flex;
   padding: 20px;
+  margin-bottom: 50px;
 `;
 
 const MyProfileImg = styled.div`
   width: 220px;
-  height: 300px;
+  height: 220px;
   overflow: hidden;
-  border-radius: 50px;
+  border-radius: 50%;
   background-color: #39bc56;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
-// const MyInfo = styled.div`
-//   display: flex;
-// `;
-
 const MyProfile = styled.div`
-  width: 500px;
+  width: 400px;
   height: 150px;
-  margin-left: 20px;
-  border: 1px solid #39bc56;
+  margin-left: 50px;
+  background-color: #d3d3d3;
   text-align: left;
   padding: 20px;
   border-radius: 20px;
-  margin-top: auto;
+  margin-top: 45px;
+`;
+
+const CategoryWrapper = styled.div`
+  display: flex;
+  margin-top: 30px;
+  margin-bottom: -30px;
+`;
+
+const CategoryButton = styled.div<{ active: number }>`
+  cursor: pointer;
+  padding: 10px;
+  margin-right: 10px;
+  border-radius: 40px;
+  border: 1px solid ${(props) => (props.active ? "#39bc56" : "#999")};
+  color: ${(props) => (props.active ? "#39bc56" : "#000")};
+  font-weight: ${(props) => (props.active ? "bold" : "normal")};
+  &:hover {
+    opacity: 0.8;
+  }
 `;
 
 const MyProjectWrapper = styled.div`
@@ -132,37 +249,37 @@ const MyProjectWrapper = styled.div`
   margin-top: 50px;
 `;
 
-const MyProjectList = styled.div`
-  display: grid;
-`;
-
 const ProjectContainer = styled.div`
   display: flex;
-  //align-items: center;
-  //justify-content: space-between;
-  //background-color: #e7f7e2;
-  border: 1px solid #39bc56;
+  background-color: #d3d3d3;
   padding: 10px;
-  padding-bottom: 10px;
   border-radius: 15px;
   cursor: pointer;
 `;
 
-const ProjectList = styled.div`
+const ProjectTitle = styled.div`
   width: 600px;
+  flex-grow: 1;
+  padding: 10px;
+`;
+
+const MoveToDetail = styled.span`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding-right: 10px;
+  font-weight: bold;
+  cursor: pointer;
+`;
+
+const ProjectSummaryContainer = styled.div`
+  width: 700px;
+  padding: 10px;
   flex-grow: 1;
 `;
 
 const ProjectSummary = styled.div`
-  width: 700px;
-  margin-top: 10px;
-  background-color: green;
-  // flex-grow: 1;
-`;
-
-const MoveToDetail = styled.span`
-  font-weight: bold;
-  cursor: pointer;
+  margin: 10px;
 `;
 
 export default MyProject;
