24256f7939ffbf89668a98595b832328eee40116
roqkfw <hyerim2888@naver.com>
2024-04-08 20:43:26 +0900

    Feat : 홈 유저조회리스트 API 연결

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,148 +5,105 @@ import axios from "axios";
 
 type Props = {};
 
+type UserType = {
+  email: string;
+  id: number;
+  introduction: string;
+  nickname: string;
+  password: string;
+  userImgs: { imgSrc: string; userImgId: number }[];
+};
 const Home = (props: Props) => {
-  const [userInfo, setUserInfo] = useState(home_layouts);
+  const [userList, setUserList] = useState<UserType[]>([]);
+  console.log(userList);
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    console.log(token);
+    axios
+      .get(`${process.env.REACT_APP_API_URL}/user/list`, {
+        headers: {
+          Authorization: token,
+        },
+      })
+      .then((res) => setUserList(res.data.userList));
+
+    return () => {};
+  }, []);
 
   return (
     <HomePage>
       <Header type="Logout" />
-      <PortfolioWrapper>
-        {userInfo.map((item, idx) => {
+      <ProfileWrapper>
+        {userList.map((item, idx) => {
           return (
-            <div key={idx}>
-              <Name>{item.name}</Name>
-              <PortfolioContant>
-                <LeftBtn></LeftBtn>
-                <RightBtn></RightBtn>
-                <ImgContent>
-                  <Imgwrapper>
-                    {item.img.map((img, idx) => {
-                      return <PortfolioImg key={idx}>{img}</PortfolioImg>;
-                    })}
-                  </Imgwrapper>
-                </ImgContent>
-              </PortfolioContant>
-            </div>
+            <ProfileContent key={idx}>
+              {item.userImgs.length > 0 ? (
+                <ProfileImg
+                  style={{ backgroundImage: `url(${item.userImgs[0].imgSrc})` }}
+                />
+              ) : (
+                <ProfileImg
+                  style={{
+                    backgroundImage: `url("./assets/join/profile.png")`,
+                  }}
+                />
+              )}
+
+              <Nickname>{item.nickname}</Nickname>
+              <Introduction>{item.introduction}</Introduction>
+              <AllPortpolio>전체 포트폴리오 보기</AllPortpolio>
+            </ProfileContent>
           );
         })}
-      </PortfolioWrapper>
+      </ProfileWrapper>
     </HomePage>
   );
 };
 
 export default Home;
-const home_layouts = [
-  {
-    name: "서혜림",
-    img: [
-      "준비된 포폴이 아직 없습니다1",
-      "준비된 포폴이 아직 없습니다2",
-      "준비된 포폴이 아직 없습니다3",
-      "준비된 포폴이 아직 없습니다4",
-      "준비된 포폴이 아직 없습니다5",
-      "준비된 포폴이 아직 없습니다6",
-      "준비된 포폴이 아직 없습니다7",
-    ],
-  },
-  {
-    name: "박지혜",
-    img: [
-      "준비된 포폴이 아직 없습니다1",
-      "준비된 포폴이 아직 없습니다2",
-      "준비된 포폴이 아직 없습니다3",
-      "준비된 포폴이 아직 없습니다4",
-      "준비된 포폴이 아직 없습니다5",
-      "준비된 포폴이 아직 없습니다6",
-      "준비된 포폴이 아직 없습니다7",
-    ],
-  },
-  {
-    name: "이민서",
-    img: [
-      "준비된 포폴이 아직 없습니다1",
-      "준비된 포폴이 아직 없습니다2",
-      "준비된 포폴이 아직 없습니다3",
-      "준비된 포폴이 아직 없습니다4",
-      "준비된 포폴이 아직 없습니다5",
-      "준비된 포폴이 아직 없습니다6",
-      "준비된 포폴이 아직 없습니다7",
-    ],
-  },
-  {
-    name: "이지은",
-    img: [
-      "준비된 포폴이 아직 없습니다1",
-      "준비된 포폴이 아직 없습니다2",
-      "준비된 포폴이 아직 없습니다3",
-      "준비된 포폴이 아직 없습니다4",
-      "준비된 포폴이 아직 없습니다5",
-      "준비된 포폴이 아직 없습니다6",
-      "준비된 포폴이 아직 없습니다7",
-    ],
-  },
-];
+
 const HomePage = styled.div`
   height: 100%;
 `;
-const PortfolioWrapper = styled.div`
-  padding: 14rem 8.4rem 7.4rem 8.4rem;
+const ProfileWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  gap: 15rem;
+`;
+const ProfileContent = styled.div`
   display: flex;
   flex-direction: column;
-  gap: 2.5rem;
+  gap: 1.2rem;
+  align-items: center;
 `;
-const PortfolioContant = styled.div`
-  background-color: #39bc56;
-  border-radius: 1rem;
-  padding: 1rem 6rem;
-  width: 100%;
+const ProfileImg = styled.div`
+  width: 20rem;
   height: 20rem;
-  box-sizing: border-box;
-  position: relative;
-  overflow: hidden;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+  border-radius: 50rem;
 `;
-const ImgContent = styled.div`
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
+const Nickname = styled.div`
+  font-size: 1.6rem;
+  font-weight: 600;
 `;
-const Imgwrapper = styled.div`
-  width: fit-content;
-  height: 100%;
-  display: flex;
-  gap: 2rem;
-`;
-const PortfolioImg = styled.div`
-  width: 27.3rem;
-  background-color: white;
-  border-radius: 1rem;
+const Introduction = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-`;
-const LeftBtn = styled.div`
-  width: 6rem;
-  height: 6rem;
-  background-image: url("./assets/home/arrow.png");
-  position: absolute;
-  background-position: 163px 235px;
-  top: 7rem;
-  left: 0;
-  cursor: pointer;
-`;
-const RightBtn = styled.div`
-  width: 6rem;
-  height: 6rem;
-  background-image: url("./assets/home/arrow.png");
-  position: absolute;
-  background-position: -8px 109px;
-  top: 7rem;
-  left: 121.2rem;
-  cursor: pointer;
-`;
-
-const Name = styled.div`
   font-size: 1.6rem;
-  font-weight: 700;
-  padding-bottom: 0.4rem;
+  border: 1px solid #39bc56;
+  width: 20rem;
+  height: 10rem;
+  border-radius: 1rem;
+  text-align: center;
+  padding: 0.5rem;
+  box-sizing: border-box;
+`;
+const AllPortpolio = styled.div`
+  font-size: 1.6rem;
+  cursor: pointer;
 `;
