5694b570fbe3f33c501dc43901eacd87a5c8455c
Seohyerim <hyerim2888@naver.com>
2024-04-08 20:55:54 +0900

    Feature/join : 회원가입 프로필 이미지 업로드, 자기소개 추가 (#20)
    * Design : UI 디자인 변경
    
    * Feat : 회원가입 프로필 이미지 업로드, 자기소개 추가

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,20 +1,31 @@
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import styled from "styled-components";
 import Header from "../../components/Header/Header";
+import axios from "axios";
 
 type Props = {};
 
 const Home = (props: Props) => {
+  const [userInfo, setUserInfo] = useState(home_layouts);
+
   return (
     <HomePage>
-      <Header />
+      <Header type="Logout" />
       <PortfolioWrapper>
-        {home_layouts.map((item, idx) => {
+        {userInfo.map((item, idx) => {
           return (
-            <div>
+            <div key={idx}>
               <Name>{item.name}</Name>
               <PortfolioContant>
-                <PortfolioImg />
+                <LeftBtn></LeftBtn>
+                <RightBtn></RightBtn>
+                <ImgContent>
+                  <Imgwrapper>
+                    {item.img.map((img, idx) => {
+                      return <PortfolioImg key={idx}>{img}</PortfolioImg>;
+                    })}
+                  </Imgwrapper>
+                </ImgContent>
               </PortfolioContant>
             </div>
           );
@@ -28,12 +39,51 @@ export default Home;
 const home_layouts = [
   {
     name: "서혜림",
+    img: [
+      "준비된 포폴이 아직 없습니다1",
+      "준비된 포폴이 아직 없습니다2",
+      "준비된 포폴이 아직 없습니다3",
+      "준비된 포폴이 아직 없습니다4",
+      "준비된 포폴이 아직 없습니다5",
+      "준비된 포폴이 아직 없습니다6",
+      "준비된 포폴이 아직 없습니다7",
+    ],
   },
   {
     name: "박지혜",
+    img: [
+      "준비된 포폴이 아직 없습니다1",
+      "준비된 포폴이 아직 없습니다2",
+      "준비된 포폴이 아직 없습니다3",
+      "준비된 포폴이 아직 없습니다4",
+      "준비된 포폴이 아직 없습니다5",
+      "준비된 포폴이 아직 없습니다6",
+      "준비된 포폴이 아직 없습니다7",
+    ],
   },
   {
     name: "이민서",
+    img: [
+      "준비된 포폴이 아직 없습니다1",
+      "준비된 포폴이 아직 없습니다2",
+      "준비된 포폴이 아직 없습니다3",
+      "준비된 포폴이 아직 없습니다4",
+      "준비된 포폴이 아직 없습니다5",
+      "준비된 포폴이 아직 없습니다6",
+      "준비된 포폴이 아직 없습니다7",
+    ],
+  },
+  {
+    name: "이지은",
+    img: [
+      "준비된 포폴이 아직 없습니다1",
+      "준비된 포폴이 아직 없습니다2",
+      "준비된 포폴이 아직 없습니다3",
+      "준비된 포폴이 아직 없습니다4",
+      "준비된 포폴이 아직 없습니다5",
+      "준비된 포폴이 아직 없습니다6",
+      "준비된 포폴이 아직 없습니다7",
+    ],
   },
 ];
 const HomePage = styled.div`
@@ -48,17 +98,53 @@ const PortfolioWrapper = styled.div`
 const PortfolioContant = styled.div`
   background-color: #39bc56;
   border-radius: 1rem;
-  padding: 2.5rem 9.9rem;
+  padding: 1rem 6rem;
   width: 100%;
-  height: 18rem;
+  height: 20rem;
   box-sizing: border-box;
+  position: relative;
+  overflow: hidden;
+`;
+const ImgContent = styled.div`
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`;
+const Imgwrapper = styled.div`
+  width: fit-content;
+  height: 100%;
+  display: flex;
+  gap: 2rem;
 `;
 const PortfolioImg = styled.div`
+  width: 27.3rem;
   background-color: white;
-  width: 100%;
-  height: 13rem;
   border-radius: 1rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
+const LeftBtn = styled.div`
+  width: 6rem;
+  height: 6rem;
+  background-image: url("./assets/home/arrow.png");
+  position: absolute;
+  background-position: 163px 235px;
+  top: 7rem;
+  left: 0;
+  cursor: pointer;
+`;
+const RightBtn = styled.div`
+  width: 6rem;
+  height: 6rem;
+  background-image: url("./assets/home/arrow.png");
+  position: absolute;
+  background-position: -8px 109px;
+  top: 7rem;
+  left: 121.2rem;
+  cursor: pointer;
+`;
+
 const Name = styled.div`
   font-size: 1.6rem;
   font-weight: 700;
