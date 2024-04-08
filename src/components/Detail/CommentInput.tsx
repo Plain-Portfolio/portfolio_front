c2755b776457c85e3153fa5bc8979442892bfec7
bgs-study <qkrwlgp235@gmail.com>
2024-04-08 21:32:21 +0900

    Feat: 대댓글 생성 구현

--- src/components/Detail/CommentInput.tsx
+++ src/components/Detail/CommentInput.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useCommentData } from "../../hooks/commenthooks";
 import { Icomment } from "../../interfaces/IDetail";
 import { FillButton, Input } from "../CommonTag";
@@ -19,6 +19,8 @@ const CommentInput = ({
   const params = useParams();
   const projectId = params.id;
 
+  const inputRef = useRef<HTMLInputElement>(null);
+
   const { createCommentMutate } = useCommentData(projectId);
 
   const handleCommentSubmit = (event: React.FormEvent<HTMLFormElement>) => {
@@ -27,9 +29,10 @@ const CommentInput = ({
       currentTarget: { content },
     } = event;
 
-    console.log(!parentCommentOrderId && commentOrder && childCommentCount);
-    console.log(!parentCommentOrderId, commentOrder, childCommentCount);
-
+    // console.log(!parentCommentOrderId && commentOrder && childCommentCount);
+    // console.log(!parentCommentOrderId, commentOrder, childCommentCount);
+    // console.log("댓글 생성 parentCommentOrderId", parentCommentOrderId);
+    // memo:지혜 최상단 컴포넌트
     if (parentCommentOrderId === null) {
       const data: Icomment = {
         context: content.value,
@@ -37,17 +40,40 @@ const CommentInput = ({
         parentCommentOrderId,
         commentOrder,
         childCommentCount,
-        isDeleted: true,
+        isDeleted: false,
       };
-      console.log(data);
+      // console.log("postform comment", data);
       createCommentMutate(data);
+    }
+
+    //memo 지혜: 대댓글 (reply)
+    else {
+      const data: Icomment = {
+        context: content.value,
+        projectId: Number(projectId),
+        parentCommentOrderId,
+        commentOrder: childCommentCount, // index!
+        childCommentCount: 0,
+        isDeleted: false,
+      };
+
+      console.log("postform reply", data);
+      createCommentMutate(data);
+    }
+    if (inputRef.current) {
+      inputRef.current.value = "";
     }
   };
 
   return (
     <Form onSubmit={handleCommentSubmit}>
       <input type="hidden" name="commentId" />
-      <Input type="text" name="content" placeholder="댓글을 입력하세요" />
+      <CInput
+        type="text"
+        name="content"
+        placeholder="댓글을 입력하세요"
+        ref={inputRef}
+      />
       <CommentSubmitBtn type="submit" value="댓글" />
     </Form>
   );
@@ -59,3 +85,7 @@ const Form = styled.form`
   display: flex;
 `;
 const CommentSubmitBtn = styled(FillButton)``;
+const CInput = styled(Input)`
+  padding-right: 1rem;
+  background-color: white;
+`;
