1de27b76abc167cfb5b5f7b0c0e664c999821f91
JIHYE PARK <79756267+j-wisdom-h@users.noreply.github.com>
2024-04-09 23:01:43 +0900

    Feature/login : 카카오 소셜로그인 구현. pretendard 폰트 적용 (#27)
    * Fix: 카카오 소셜로그인 에러
    
    * Feat: 카카오 소셜로그인
    
    * Feat: pretendard font적용

--- src/styles/GlobalStyels.ts
+++ src/styles/GlobalStyels.ts
@@ -1,14 +1,17 @@
 import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
+import { fonts } from "./fonts";
 
 const GlobalStyles = createGlobalStyle`
     ${reset}
+    ${fonts}
     a{
         text-decoration: none;
         color: inherit;
     }
     *{
         box-sizing: border-box;
+       
     }
     html, body, div, span, h1, h2, h3, h4, h5, h6, p, 
     a, dl, dt, dd, ol, ul, li, form, label, table{
@@ -29,9 +32,11 @@ const GlobalStyles = createGlobalStyle`
         width:100%;
         height:100%;
         line-height: 1;
-        font-family: 'Noto Sans KR', sans-serif;
         background-color: #fff;
         margin-bottom: 100px;
+    }
+    html, body, input, button, textarea, li {
+        font-family: "Pretendard Variable", Pretendard, -apple-system, BlinkMacSystemFont, system-ui, Roboto, "Helvetica Neue", "Segoe UI", "Apple SD Gothic Neo", "Noto Sans KR", "Malgun Gothic", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", sans-serif;
     }
     ol, ul{
         list-style: none;
@@ -41,6 +46,7 @@ const GlobalStyles = createGlobalStyle`
         background: transparent;
         cursor: pointer;
     }
+  
 `;
 
 export default GlobalStyles;
