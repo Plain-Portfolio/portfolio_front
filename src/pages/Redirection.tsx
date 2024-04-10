1bc4326a6cb54faa6eeef0c963415e942eeadd5b
bgs-study <qkrwlgp235@gmail.com>
2024-04-10 20:18:38 +0900

    Fix: 카카오 소셜 로그인 환경변수 변경 및 context로그인 연결

--- src/pages/Redirection.tsx
+++ src/pages/Redirection.tsx
@@ -1,11 +1,14 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useContext, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import Layout from "../components/Layout/Layout";
+import { AuthContext } from "../components/Context/AuthContext";
 
 type Props = {};
 
 const Redirection = (props: Props) => {
   const navigate = useNavigate();
+  const { login } = useContext(AuthContext);
 
   const code = new URL(document.location.toString()).searchParams.get(
     "code"
@@ -17,7 +20,7 @@ const Redirection = (props: Props) => {
       try {
         const response = await axios.post(
           "https://kauth.kakao.com/oauth/token",
-          `grant_type=authorization_code&client_id=${process.env.REST_KAKAO_API_KEY}&redirect_uri=${process.env.REDIRECT_KAKAO_URI}&code=${code}`,
+          `grant_type=authorization_code&client_id=${process.env.REACT_APP_REST_KAKAO_API_KEY}&redirect_uri=${process.env.REACT_APP_REDIRECT_KAKAO_URI}&code=${code}`,
           {
             headers: {
               "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
@@ -39,21 +42,33 @@ const Redirection = (props: Props) => {
           );
 
           const { profile } = userResponse.data.kakao_account;
+
           const { nickname, email } = profile;
           console.log(profile, nickname, email);
 
           const postData = {
             nickname,
-            ...(email && { email }), // Add email only if it exists
+            email,
           };
 
-          const res = await axios.post(
-            `${process.env.REACT_APP_API_URL}/user/login/kakao/callback`,
-            postData
-          );
-          const result = res.data;
-          localStorage.setItem("nickname", result.nickname);
-          console.log(result);
+          const res = await axios
+            .post(
+              `${process.env.REACT_APP_API_URL}/user/login/kakao/callback`,
+              postData
+            )
+            .then((res) => {
+              const data = res.data;
+
+              login({
+                token: data.token,
+                user: {
+                  userId: String(data.userId),
+                  email: data.email,
+                  nickname: data.nickname,
+                },
+              });
+              navigate("/");
+            });
         }
       } catch (error) {
         console.error("Error during Kakao login:", error);
@@ -63,7 +78,11 @@ const Redirection = (props: Props) => {
     handleKakaoLogin(code);
   }, []);
 
-  return <div>recirection</div>;
+  return (
+    <Layout>
+      <div>카카오 소셜 로그인 중입니다. 잠시만 기다려주세요.</div>
+    </Layout>
+  );
 };
 
 export default Redirection;
